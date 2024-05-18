b29c70885f2d4f478353dbc1a384fab0f1b25c9a
3bood-kr <rawashdeh250@gmail.com>
2024-05-18 15:41:50 +0300

    fixed ajax and widget

--- amd/src/modaldialouge.js
+++ amd/src/modaldialouge.js
@@ -1,14 +1,37 @@
 import ModalFactory from 'core/modal_factory';
+import {call as ajaxCall} from 'core/ajax';
 import Templates from 'core/templates';
+import {add as addToast} from 'core/toast';
+import {get_string as getString} from 'core/str';
+
+const addNotification = (msg, type) => {
+    addToast(msg, {type: type});
+};
 
 export const init = (linkSelector) => {
-    document.querySelector(linkSelector).addEventListener('click', async (e) => {
-        const modal = await ModalFactory.create({
-            large: true,
-            title: 'test title',
-            body: '<p>Example body content</p>',
-            footer: 'An example footer content',
-        });
-        modal.show();
+    document.querySelector(linkSelector).addEventListener('click', (e) => {
+        e.preventDefault();
+        ajaxCall([{
+            methodname: 'local_tickets_get_tickets', 
+            args: {},
+            done: async function(response) {
+                if(response.status == 200){
+                    const tickets = JSON.parse(response.data);
+                    const modal = await ModalFactory.create({
+                        large: true,
+                        title: getString('manage_tickets', 'local_tickets'),
+                        body: Templates.render('local_tickets/modaldialouge', {tickets: tickets}),
+                    });
+                    modal.show();
+                }
+                else{
+                    addNotification(response.message, 'danger');
+                }
+            },
+            fail: function(ex) {
+                addNotification(`Failed to fetch ticket: ${ex}`, 'danger');
+                console.log(ex);
+            }
+        }])
     });
 };
