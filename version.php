ac16aeecf531e175bceda755d1a242a47316c9a6
3bood-kr <rawashdeh250@gmail.com>
2024-07-27 23:05:26 +0300

    grunt and plugin version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version  = 2024072701;
-$plugin->requires = 2014051200;
-$plugin->component = 'local_tickets';
-$plugin->release = '1.0';
-$plugin->maturity = MATURITY_STABLE;
+$plugin->requires = 2022111500;
+$plugin->component = 'local_tickets';  
+$plugin->release = '1.0'; 
+$plugin->maturity = MATURITY_STABLE; 
